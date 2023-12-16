54764856438562f085a4c546bc1459da1d10f596
Menonetes <schmidseb@protonmail.com>
2023-12-16 22:25:34 +0100

    add Controller. mov code out of main into controller.

---
+++ AdventOfCode2023/src/Controller.java
@@ -0,0 +1,9 @@
+public class Controller {
+    public void start(){
+        System.out.println("Day1Part1: " + new day1.Part1().getCalibrationValue());
+        System.out.println("Day1Part2: " + new day1.Part2().getCalibrationValue());
+
+        System.out.println("Day2Part1: " + new day2.Part1().getSum());
+        System.out.println("Day2Part2: " + new day2.Part2().getSumOfPower());
+    }
+}
--- AdventOfCode2023/src/day3/Part1.java
+++ AdventOfCode2023/src/day3/Part1.java
@@ -1,4 +1,10 @@
 package day3;
 
 public class Part1 {
+    public Integer getsum(){
+        Integer sum = 0;
+
+
+        return sum;
+    }
 }
